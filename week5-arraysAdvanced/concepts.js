092305f08580eaac081c7b743ce9d076313a99b8
Petya Ferreira <petianaidenova@gmail.com>
2021-10-11 22:18:10 +0200

    end of lecture

--- week5-arraysAdvanced/concepts.js
+++ week5-arraysAdvanced/concepts.js
@@ -17,3 +17,13 @@
 // UI Frameworks : React, Angular, Vue - started software that i can finish (osnovite na sgradata za slozheni i az sama si q dovyrshvam)
 // UI libraries: Kendo UI, Sencha - they provide functionality and components (nqkoi mi dava prozorez ili mivka)
 
+// Software engineering
+// Requirements analysis, software design, construction, testing
+
+//QA - quality ensurance. Softawre to be working properly and to do this that it is designed to do
+// QA engineer is a programmer who does testing
+
+// Unit test framework
+// Mocha for JS
+
+// Trello - system for managing projects
