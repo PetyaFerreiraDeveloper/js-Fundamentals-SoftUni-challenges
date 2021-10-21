13a00abd4c0ffcc83e008393242a8d2a7a83149f
Petya Ferreira <petianaidenova@gmail.com>
2021-10-21 11:10:41 +0200

    02. Programming Fundamentals Mid Exam

--- week5-arraysAdvanced/arrays-advanced-lab/test.js
+++ week5-arraysAdvanced/arrays-advanced-lab/test.js
@@ -1,9 +1,10 @@
 function arraysDemo7() {
     let arr = [1, 2, 3, 4, 5];
     let newArr = [6, 6, 6];
-    arr = newArr.slice(0);
+    arr.splice(1, 0, 6, 5)
     console.log(arr);
     
 }
 
 arraysDemo7()
+console.log("Sorry you lose :(");
